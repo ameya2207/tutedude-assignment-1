c7f71f19aa1da0eca6b95b2e052fa6a933b7f45a
ameya2207 <156674246+ameya2207@users.noreply.github.com>
2025-08-12 16:58:55 +0530

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first name: ").strip()
+last_name = input("Enter your last name: ").strip()
+
+full_name = f"{first_name} {last_name}"
+
+print(f"Hello, {full_name}! Welcome to the Python programming world.")
